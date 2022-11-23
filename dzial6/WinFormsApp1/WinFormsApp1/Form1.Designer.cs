c15c8cb64a5e473e64899cc773eb047eda3ad347
koper <janul.koper@gmail.com>
2022-11-23 15:01:15 +0100

    koniec

--- dzial6/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ dzial6/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     partial class Form1
@@ -29,13 +31,140 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.assignJob = new System.Windows.Forms.Button();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.report = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zadanie robotnicy";
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Nauczanie pszczółek",
+            "Pielęgnacja jaj",
+            "Utrzymanie ula",
+            "Wytwarzanie miodu",
+            "Zbieranie nektaru",
+            "Patrol z żądłami"});
+            this.workerBeeJob.Location = new System.Drawing.Point(5, 63);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(121, 23);
+            this.workerBeeJob.TabIndex = 1;
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(132, 64);
+            this.shifts.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(86, 23);
+            this.shifts.TabIndex = 2;
+            this.shifts.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zmiany";
+            // 
+            // assignJob
+            // 
+            this.assignJob.Location = new System.Drawing.Point(6, 106);
+            this.assignJob.Name = "assignJob";
+            this.assignJob.Size = new System.Drawing.Size(213, 23);
+            this.assignJob.TabIndex = 4;
+            this.assignJob.Text = "Przypisz tę pracę pszczole";
+            this.assignJob.UseVisualStyleBackColor = true;
+            this.assignJob.Click += new System.EventHandler(this.assignJob_Click);
+            // 
+            // nextShift
+            // 
+            this.nextShift.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nextShift.Location = new System.Drawing.Point(260, 10);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(212, 131);
+            this.nextShift.TabIndex = 5;
+            this.nextShift.Text = "Przepracuj następną zmianę";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.workerBeeJob);
+            this.groupBox1.Controls.Add(this.assignJob);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.shifts);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 149);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Przydział prac robotnicom";
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(0, 167);
+            this.report.Multiline = true;
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(483, 227);
+            this.report.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 400);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.nextShift);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private ComboBox workerBeeJob;
+        private NumericUpDown shifts;
+        private Label label2;
+        private Button assignJob;
+        private Button nextShift;
+        private GroupBox groupBox1;
+        private TextBox report;
     }
 }
 
--- dzial6/WinFormsApp1/WinFormsApp1/Queen.cs
+++ dzial6/WinFormsApp1/WinFormsApp1/Queen.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace WinFormsApp1
 {
-    class Queen
+    public class Queen
     {
-        public Queen(Workers[] workers)
+        public Queen(Worker[] workers)
         {
             this.workers = workers;
         }
@@ -14,24 +14,24 @@ namespace WinFormsApp1
         private int shiftNumber = 0;
         public bool AssignWork(string job, int numberOfShifts)
         {
-            for(int i=0; i < workers.Length; i++)           
+            for (int i = 0; i < workers.Length; i++)
                 if (workers[i].DoThisJob(job, numberOfShifts))
                     return true;
-                return false;            
+            return false;
         }
         public string WorkTheNextShift()
         {
             shiftNumber++;
             string report = "Raport zmiany numer " + shiftNumber + "\r\n";
-            for(int i = 0; i < workers.Length; i++)
+            for (int i = 0; i < workers.Length; i++)
             {
                 if (workers[i].DidYouFinish())
                     report += "Robotnica nr " + (i + 1) + " zakonczyla swoje zadanie\r\n";
                 if (String.IsNullOrEmpty(workers[i].CurrentJob))
                     report += "Robotnica nr " + (i + 1) + " nie pracuje\r\n";
                 else
-                    if (workers[i].ShiftLeft > 0)
-                    report += "Robotnica numer " + (i + 1) + " robi " + workers[i].CurrentJob + "jeszcze przez " + workers[i].ShiftLeft + " zmiany\r\n";
+                    if (workers[i].ShiftsLeft > 0)
+                    report += "Robotnica numer " + (i + 1) + " robi " + workers[i].CurrentJob + "jeszcze przez " + workers[i].ShiftsLeft + " zmiany\r\n";
                 else
                     report += "Robotnica numer " + (i + 1) + " zakonczy " + workers[i].CurrentJob + "po tej zmiany \r\n";
             }
