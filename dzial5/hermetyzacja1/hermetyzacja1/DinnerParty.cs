114ddb0dcd98599e97dbfcd36f0631aec19e16d2
koper <janul.koper@gmail.com>
2022-11-23 10:53:27 +0100

    hermetyzacja

---
+++ dzial5/hermetyzacja1/hermetyzacja1/DinnerParty.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hermetyzacja1
+{
+    class DinnerParty
+    {
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public bool HealthyOption { get; set; }
+        public const int CostOfFoodPerPerson = 25;
+        public DinnerParty(int numberOfPeople,bool healthyOption,bool fancyDecorations)
+        {
+            
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            HealthyOption = healthyOption;
+        }
+        private decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+            if (FancyDecorations)
+            {
+                costOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+
+            }
+            else
+            {
+                costOfDecorations = (NumberOfPeople * 7.50M) + 30M;
+            }
+            return costOfDecorations;
+        }
+        private decimal CalculateCostOfBeveragesPerPerson()
+        {
+            decimal costOfBeverages;
+            if (HealthyOption)
+            {
+                costOfBeverages = 5.00M;
+
+            }
+            else
+            {
+                costOfBeverages = 20.00M;
+            }
+            return costOfBeverages;
+        }
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += ((CalculateCostOfBeveragesPerPerson() + CostOfFoodPerPerson) * NumberOfPeople);
+                if (HealthyOption)
+                {
+                    totalCost *= .95M;
+                }
+                return totalCost;
+            }
+        }
+        }
+        
+
+    }
+}
---
+++ dzial5/hermetyzacja1/hermetyzacja1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hermetyzacja1
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerparty;
+        public Form1()
+        {
+
+            InitializeComponent();
+            dinnerparty = new DinnerParty((int)numericUpDown1.Value,healthyBox.Checked,fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+        
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerparty.Cost;
+            costLabel.Text = Cost.ToString("c");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerparty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.FancyDecorations = fancyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerparty.HealthyOption = healthyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+    }
+}
